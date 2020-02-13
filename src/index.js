8c9103c65e6b32df9f0b990a50906b3318c6e82e
Harsha <nagaharshavardhan.nsm@gmail.com>
2020-02-13 11:50:27 -0800

    Styling in JSX format

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Create React Component
 const App = function() {
-    return <div>Hi There</div>;
+    return (
+        <div>
+            <label>
+                Enter Name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor:'blue', color:'white'}}>Submit</button>
+        </div>
+    )
 }
 
+// Display React component on the Screen
 ReactDOM.render(
     <App />,
     document.querySelector("#root")

f2f0cb1223d4b08a6646316b87ffaf3974d11f1e
Harsha <nagaharshavardhan.nsm@gmail.com>
2020-02-14 11:23:44 -0800

    Access the javascript variables im the jsx

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom'
 
 // Create React Component
 const App = function() {
+    const button = "Click here!!"
     return (
         <div>
-            <label>
+            <label className="label">
                 Enter Name:
             </label>
             <input id="name" type="text" />
-            <button style={{backgroundColor:'blue', color:'white'}}>Submit</button>
+            <button style={{backgroundColor:'blue', color:'white'}}>
+                {button}
+            </button>
         </div>
     )
 }
